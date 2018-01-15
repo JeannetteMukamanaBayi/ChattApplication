fd351cf454cfd78ff98bbd5969205c8a960d8d91
Jeannette Mukamana <jmukamana@go.rwanda.cmu.local>
2018-01-15 19:31:25 +0200

    my first commit

--- Server.js
+++ Server.js
@@ -1,14 +1,20 @@
 var express = require('express');
+
+//handling post requests
 var bodyParser = require('body-parser');
 var urlencoded = bodyParser.urlencoded({ extended: false });
 var app = express();
+
+//setting  redndering dynamic web pages
+app.set('view engine', 'ejs');
+
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var users = [];
 var connections = [];
 const mysql = require('mysql');
 const router = express.Router();
-
+let mySocket = null ;
 
 //creating a connection to database
 const databaseConnection = mysql.createConnection({
@@ -48,71 +54,96 @@ app.get('/register', function (req, res) {
 
 //handling requests for registering new user
 app.post('/register', urlencoded, function (req, res) {
-    let user = { FirstName: req.body.fname, LastName: req.body.lname, Email: req.body.email, userName: req.body.userName, password: req.body.pwd };
-    let sql = 'INSERT INTO users SET ?'
-    let query = databaseConnection.query(sql, user, (err, result) => {
+
+    //check if the user name already exist
+    let sql = 'SELECT *  FROM users WHERE  userName  = "' + req.body.userName + '"';
+    let query = databaseConnection.query(sql, (err, result) => {
         if (err) {
-            res.sendFile(__dirname + '/register.html');
+            console.log("enable to register the user");
             console.log(err);
         }
+
+        if (result[0] != undefined) {
+            console.log(req.body.userName + "Already exists");
+            //sending back the registration form if the user already exists     
+            res.sendFile(__dirname + '/register.html');
+        }
         else {
-            res.sendFile(__dirname + '/index.html');
+
+            //adding the user into the databse if the users doen not already exists
+            let user = { FirstName: req.body.fname, LastName: req.body.lname, Email: req.body.email, userName: req.body.userName, password: req.body.pwd };
+            let sql = 'INSERT INTO users SET ?'
+            let query = databaseConnection.query(sql, user, (err, result) => {
+                if (err) {
+                    res.sendFile(__dirname + '/register.html');
+                    console.log(err);
+                }
+
+                //return the home page after user's successfull registration
+                else {
+                    res.sendFile(__dirname + '/index.html');
+                }
+            });
         }
     });
 })
 
 //handling requests for user loging
 app.post('/login', urlencoded, function (req, res) {
+    let userName = req.body.userName;
     //checking if the user exisists and all credentials maches
     let sql = 'SELECT *  FROM users WHERE  userName  = "' + req.body.userName + '"   AND password = "' + req.body.pwd + '"';
     let query = databaseConnection.query(sql, (err, result) => {
+        //alert the user if credentials do not match
+
         if (err) {
             console.log("enable to identify the user");
             console.log(err);
         }
+        //feching all messages after the user successfully logged in
+
+       
         else {
-            console.log(result[0].userName);
-            //return this page only if the user is successfully logged in
-            res.sendFile(__dirname + '/chatRoom.html');
+            mySocket.emit('new user', userName); 
+            let sql = 'SELECT *  FROM messages';
+            let query = databaseConnection.query(sql, (err, result) => {
+                if (err) console.log("enable to fecht messages");
+                else {
+                    var result2 = JSON.stringify(result);
+                    res.render(__dirname + '/chatRoom', { userName: req.body.userName, oldMessages: result2 });
+
+                }
+            });
 
         }
     });
 
 })
 
-
 //accepting new client connection and add it to the list of connected client
 io.sockets.on('connection', function (socket) {
+    mySocket = socket;
     connections.push(socket);
+
     //display the number of connected users
     console.log('# of connected users is %s', connections.length);
-
-    //feching all posts when the user is connected
-    let sql = 'SELECT *  FROM messages';
-    let query = databaseConnection.query(sql, (err, result) => {
-        if (err) console.log("enable to fecht messages");
-        else {
-            console.log(result[0].content);
-
-            io.sockets.emit('all messages', { msg: result });
-        }
-    });
-
-
     //disconnecting fromt the server
     socket.on('disconnect', function (data) {
         connections.splice(connections.indexOf(socket), 1);
         console.log('Remaining # of connected users is %s', connections.length);
+        socket.emit('user left', data.userName); 
     });
 
     //sending  and saving messages 
     socket.on('send message', function (data) {
-        console.log(data);
+
         //sending messages to connected client
-        io.sockets.emit('new message', { msg: data });
+        let timeStamp = new Date();
+        io.sockets.emit('new message', { message: data, time: timeStamp.getDate() + "/" + timeStamp.getMonth() + 1 + "/" + timeStamp.getFullYear() + "   " + timeStamp.getHours() + ":" + timeStamp.getMinutes() });
+        console.log("timeStamp.getFullYear()" + timeStamp.getDate());
 
         //saving messages into database
-        let message = { sender: 'Jeannette', content: data, timeStamp: new Date().toLocaleString() };
+        let message = { sender: 'Jeannette', content: data, timeStamp: timeStamp.getDate() + "/" + timeStamp.getMonth() + 1 + "/" + timeStamp.getFullYear() + "   " + timeStamp.getHours() + ":" + timeStamp.getMinutes() };
         let sql = 'INSERT INTO messages SET ?'
         let query = databaseConnection.query(sql, message, (err, result) => {
             if (err) {
